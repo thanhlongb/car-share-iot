096b1083349b09f99dd5cd59b1eb6ae8018a5e28
thanhlongb <thlong.hcm@gmail.com>
2020-08-26 10:12:43 +0700

    add user login and registration features

---
+++ masterpi/app/users/constants.py
@@ -0,0 +1,11 @@
+# User role
+ADMIN = 0
+MANAGER = 1
+ENGINEER = 2
+CUSTOMER = 3
+ROLE = {
+    ADMIN: 'admin',
+    MANAGER: 'manager',
+    ENGINEER: 'engineer',
+    CUSTOMER: 'customer',
+}
---
+++ masterpi/app/users/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import Form, RecaptchaField
+from wtforms import TextField, PasswordField, BooleanField
+from wtforms.validators import Required, EqualTo, Email
+
+class LoginForm(Form):
+    email = TextField('Email address', [Required(), Email()])
+    password = PasswordField('Password', [Required()])
+
+class RegisterForm(Form):
+    name = TextField('NickName', [Required()])
+    email = TextField('Email address', [Required(), Email()])
+    password = PasswordField('Password', [Required()])
+    confirm = PasswordField('Repeat Password', [
+        Required(),
+        EqualTo('password', message='Passwords must match')
+        ])
+    accept_tos = BooleanField('I accept the TOS', [Required()])
