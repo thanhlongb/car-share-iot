5cd71ecd66ed0c245d8176c79d993c16341af5c4
thanhlongb <thlong.hcm@gmail.com>
2020-08-28 08:29:57 +0700

    add site's html template

--- masterpi/app/__init__.py
+++ masterpi/app/__init__.py
@@ -10,10 +10,12 @@ db = SQLAlchemy(app)
 def not_found(error):
     return render_template('404.html'), 404
 
+from app.site.views import mod as siteModule
 from app.users.views import mod as usersModule
 from app.users.views import api_mod as usersAPIModule
 from app.cars.views import mod as carsModule
 from app.bookings.views import mod as bookingsModule
+app.register_blueprint(siteModule)
 app.register_blueprint(usersModule)
 app.register_blueprint(usersAPIModule)
 app.register_blueprint(carsModule)
