07a32ecd4d7971f525aaced0753d4dc5568461c1
Quang Tran <s3757821@rmit.edu.vn>
2020-09-02 11:36:57 +0700

    update booking function

--- masterpi/app/bookings/forms.py
+++ masterpi/app/bookings/forms.py
@@ -2,16 +2,5 @@ from flask_wtf import Form, RecaptchaField
 from wtforms import TextField, PasswordField, BooleanField
 from wtforms.validators import Required, EqualTo, Email
 
-class LoginForm(Form):
-    username = TextField('Username', [Required()])
-    password = PasswordField('Password', [Required()])
-
-class RegisterForm(Form):
-    username = TextField('Username', [Required()])
-    email = TextField('Email address', [Required(), Email()])
-    password = PasswordField('Password', [Required()])
-    confirm = PasswordField('Repeat Password', [
-        Required(),
-        EqualTo('password', message='Passwords must match')
-        ])
-    accept_tos = BooleanField('I accept the TOS', [Required()])
+class durationForm(Form):
+    duration = TextField('duration', [Required()])
