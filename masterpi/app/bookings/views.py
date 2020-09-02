f6a607cd3ba5c25fc91ec7b915b35c0f3155b1f2
Quang Tran <s3757821@rmit.edu.vn>
2020-09-02 11:36:57 +0700

    update booking function

--- masterpi/app/bookings/views.py
+++ masterpi/app/bookings/views.py
@@ -3,26 +3,28 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 from app import db
 from app.bookings.models import Booking, BookingAction
 from app.cars.models import Car, CarLocation, CarReport
-
+from app.bookings.forms import durationForm
 
 
 mod = Blueprint('bookings', __name__, url_prefix='/bookings')
 
-
-def book_car(car_id, duration):
+mod.route('/book-car/<id>', methods=['GET'])
+def book_car(id):
+    duration = durationForm(request.form)
     car = Car.query.filter_by(id=id).first()
     if car:
         user_id = session['user_id'] #not sure 
-        booking = Booking(user_id, car_id, duration)
-
+        booking = Booking(user_id, id, duration)
+        bookingAction = BookingAction(booking.id, "book")
+        db.session.add(bookingAction)
         db.session.add(booking)
         db.session.commit()
     return render_template("404.html")
 
 
+mod.route('/cancel-booking/<booking_id>', methods=['GET'])
 def cancel_booking(booking_id):
-    booking = Booking.query.filter_by(id=id).first()
-    if booking:
-        db.session.add(booking)
-        db.session.commit()
+    bookingAction = BookingAction(booking_id, "cancel")
+    db.session.add(bookingAction)
+    db.session.commit()
     return render_template("404.html")
