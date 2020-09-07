e0fd6d0558bfb861629a217e1de56c4c31d6a4ce
Quang Tran <s3757821@rmit.edu.vn>
2020-09-08 00:52:26 +0700

    Add view and api for users function

--- masterpi/app/bookings/views.py
+++ masterpi/app/bookings/views.py
@@ -3,26 +3,24 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 from app import db
 from app.bookings.models import Booking, BookingAction
 from app.cars.models import Car, CarLocation, CarReport
-from app.bookings.forms import durationForm
-
 
 mod = Blueprint('bookings', __name__, url_prefix='/bookings')
 
-mod.route('/book-car/<id>', methods=['GET'])
-def book_car(id):
-    duration = durationForm(request.form)
-    car = Car.query.filter_by(id=id).first()
+@mod.route('/book-car', methods=['POST'])
+def book_car():
+    car = Car.query.filter_by(id=request.form['car_id']).first()
     if car:
-        user_id = session['user_id'] #not sure 
-        booking = Booking(user_id, id, duration)
+        user_id = session['user_id'] 
+        booking = Booking(user_id, car.id, request.form['duration'])
+        db.session.add(booking)
+        db.session.commit()
         bookingAction = BookingAction(booking.id, "book")
         db.session.add(bookingAction)
-        db.session.add(booking)
         db.session.commit()
     return render_template("404.html")
 
 
-mod.route('/cancel-booking/<booking_id>', methods=['GET'])
+@mod.route('/cancel-booking', methods=['POST'])
 def cancel_booking(booking_id):
     bookingAction = BookingAction(booking_id, "cancel")
     db.session.add(bookingAction)
