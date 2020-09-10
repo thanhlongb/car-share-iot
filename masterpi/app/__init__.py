53a5a6a1cac7a0926134e9dba912f59831b8f38f
thanhlongb <s3748575@rmit.edu.vn>
2020-09-11 00:08:52 +0700

    fix url structure

--- masterpi/app/__init__.py
+++ masterpi/app/__init__.py
@@ -16,12 +16,10 @@ with app.app_context():
 def not_found(error):
     return render_template('404.html'), 404
 
-from app.site.views import mod as siteModule
 from app.users.views import mod as usersModule
 from app.users.views import api_mod as usersAPIModule
 from app.cars.views import mod as carsModule
 from app.bookings.views import mod as bookingsModule
-app.register_blueprint(siteModule)
 app.register_blueprint(usersModule)
 app.register_blueprint(usersAPIModule)
 app.register_blueprint(carsModule)
