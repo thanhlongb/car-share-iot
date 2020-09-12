12cabe571f545325fb8bc2503b497f81c92265a2
trungnq0202 <s3742774@rmit.edu.vn>
2020-09-12 15:48:25 -0400

    bluetooth works, fixing bugs with parallel running threads

--- agentpi/detect_nearby_device.py
+++ agentpi/detect_nearby_device.py
@@ -1,28 +1,19 @@
 import bluetooth
 import time
-
+import requests
+import warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+warnings.simplefilter('ignore', InsecureRequestWarning)
 ENGINEER_LOGIN_BY_BLUETOOTH_API = "https://127.0.0.1:5000/api/users/engineer_unlock_car_bluetooth/"
 
 def detect():
     nearbyDevices = bluetooth.discover_devices()
     for macAddress in nearbyDevices:
-        Params = {'username' : macAddress}
+        Params = {'bluetooth_MAC' : macAddress}
         response = requests.post(ENGINEER_LOGIN_BY_BLUETOOTH_API, Params, verify=False)
         response_json = response.json()
         if len(response_json) != 0:
-            return response_json
+            return response_json['username']
     return ''
-    # while True:
-    #     nearbyDevices = bluetooth.discover_devices()
-    #     for macAddress in nearbyDevices:
-    #         Params = {'username' : macAddress}
-    #         response = requests.post(ENGINEER_LOGIN_BY_BLUETOOTH_API, Params, verify=False)
-    #         response_json = response.json()
-    #         if len(response_json) != 0:
-    #             print(response_json)
-    #             return response_json
 
-    #     time.sleep(3)
 
-# if __name__ == '__main__':
-#     detect()
