07cdddb2b1cf5d508fbd83f2d52485acca330261
Quang Tran <s3757821@rmit.edu.vn>
2020-09-15 20:28:37 +0700

    fix bug in __init__ for googlemaps

--- masterpi/app/cars/maps.py
+++ masterpi/app/cars/maps.py
@@ -1,23 +1,5 @@
 import geocoder
-from flask import Flask, render_template
-from flask_googlemaps import GoogleMaps, Map, icons
-
-@app.route("/")
-def map_created_in_view():
-
-    gmap = Map(
-        identifier="gmap",
-        varname="gmap",
-        lat=10.78403000000003,
-        lng=106.69434000000007,
-        markers={
-            # icons.dots.green: [(21.02988000000005, 105.81131000000005)],
-            icons.dots.blue: [(10.78403000000003, 106.69434000000007, "Hello World")],
-        },
-        style="height:400px;width:600px;margin:0;",
-    )
-
-    return render_template("simple.html", gmap=gmap)
+from flask_googlemaps import  Map, icons
 
 class StaticMap:
     
@@ -34,7 +16,7 @@ class StaticMap:
     def create_map(self):
         geolocation = self.get_location_coordinate()
         description = "car {}".format(car_id)
-        googleMap = Map(
+        gmap = Map(
             identifier="car location",
             varname="gmap",
             lat=geolocation[0],
@@ -44,4 +26,4 @@ class StaticMap:
             },
             style="height:400px;width:600px;margin:0;",
         )
-        return googleMap
+        return gmap
--- masterpi/config.py
+++ masterpi/config.py
@@ -37,7 +37,7 @@ CSRF_SESSION_KEY = '''lur$F1de)*Cxy;SA>fxvwqF=!R`F?m`PPO4~a;>,4t:yLEr*n6~0aStU:?
 GOOGLE_CLIENT_ID = '''798556033482-18bih5lt883gvriclvr7fdger63hcnb4.apps.googleusercontent.com'''
 GOOGLE_CLIENT_SECRET = '''3azjWEwWkaMvMHPA_ue7FfR-'''
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
-
+# GOOGLEMAPS_KEY = ""
 # RECAPTCHA_USE_SSL = False
 # RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
 # RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
